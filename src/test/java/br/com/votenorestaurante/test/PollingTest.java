e46f65c5ed8e1c928a31c3abb9584a0888be5f5f
Andre Lemos <andreasl@disoft.com.br>
2016-01-01 21:15:45 -0200

    Adicionado teste de combinações

--- src/test/java/br/com/votenorestaurante/test/PollingTest.java
+++ src/test/java/br/com/votenorestaurante/test/PollingTest.java
@@ -1,9 +1,14 @@
 package br.com.votenorestaurante.test;
 
 import br.com.votenorestaurante.model.Poll;
+import br.com.votenorestaurante.model.Restaurant;
+import br.com.votenorestaurante.service.CombinationFactory;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PollingTest extends TestCase {
@@ -23,8 +28,41 @@ public class PollingTest extends TestCase {
                 .withUser("Teste", "teste@teste.com.br")
                 .building();
 
-
         assertNotNull(poll.getRestaurant());
+    }
+
+
+    public void testValidCombination() {
+
+        List<Restaurant> restaurants = new ArrayList<Restaurant>();
+        List<Poll> polls = new ArrayList<Poll>();
+
+        for (int i = 0; i < 5; i++) {
+            Restaurant restaurant = new Restaurant();
+            restaurant.setId(new Long(i + 1));
+            restaurants.add(restaurant);
+        }
+
+        List<Restaurant> restaurantsList = null;
+
+        int i = 0;
+        do {
+            CombinationFactory combinationFactory = new CombinationFactory();
+            restaurantsList = combinationFactory.getValidCombination(restaurants, polls);
+
+            if (restaurantsList != null) {
+                Restaurant restaurant1 = restaurantsList.get(0);
+                Restaurant restaurant2 = restaurantsList.get(1);
+
+
+                polls.add(new Poll(restaurant1, restaurant2, null, null));
+            }
+
+            i++;
+        } while (restaurantsList != null);
+
+        assertEquals(11, i);
+
     }
 
 }
